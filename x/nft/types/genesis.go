df5625fe9ba97357397ffdae82af2d3ea41e0e36
Phuong Do <minhphuong.dh96@gmail.com>
2022-12-09 10:00:15 +0700

    modify nft types package

--- x/nft/types/genesis.go
+++ x/nft/types/genesis.go
@@ -1,21 +1,48 @@
 package types
 
 // this line is used by starport scaffolding # genesis/types/import
+import (
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
 
 // DefaultIndex is the default global index
 const DefaultIndex uint64 = 1
 
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
-	return &GenesisState{
-		// this line is used by starport scaffolding # genesis/types/default
-	}
+	return NewGenesisState([]Collection{})
 }
 
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
 	// this line is used by starport scaffolding # genesis/types/validate
+	for _, c := range gs.Collections {
+		if err := ValidateDenomID(c.Denom.Name); err != nil {
+			return err
+		}
+
+		for _, nft := range c.NFTs {
+			if nft.GetOwner().Empty() {
+				return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "missing owner")
+			}
+
+			if err := ValidateTokenID(nft.GetID()); err != nil {
+				return err
+			}
+
+			if err := ValidateTokenURI(nft.GetURI()); err != nil {
+				return err
+			}
+		}
+	}
 
 	return nil
+}
+
+// NewGenesisState creates a new genesis state.
+func NewGenesisState(collections []Collection) *GenesisState {
+	return &GenesisState{
+		Collections: collections,
+	}
 }
