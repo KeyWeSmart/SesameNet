faff3274a6d43bf1c7490580dbfe94bc5c2eddde
Phuong Do <minhphuong.dh96@gmail.com>
2023-05-19 00:43:24 +0700

    add script for 5 machine demo

--- deploy/testnet_config/script.sh
+++ deploy/testnet_config/script.sh
@@ -3,6 +3,7 @@ MNEMONIC0="nation glance keep family feed demand want sauce certain grit logic r
 MNEMONIC1="bind claim solve athlete record appear fruit maple strategy post absent unfold outdoor tent play evil nephew paper agent visa eye tomato plate process"
 MNEMONIC2="pledge raccoon canoe couch until time fitness legend bunker liquid become expect alcohol inherit alter theme jazz nasty sad pencil near reopen decide direct"
 MNEMONIC3="bulb bottom rough elevator note purity twice sure hill refuse galaxy body wreck lake flag sight retreat elevator country silly pink regret assist dove"
+MNEMONIC4="squeeze online orchard option tiny duty vapor body learn ensure term anger space miracle nerve clerk circle tonight chair desk steel burst glare accident"
 TREASURER="medal purchase daughter execute please bind that switch seek history much success dice bird clarify luxury devote coast labor piano dinner young marine inspire"
 
 
@@ -11,12 +12,14 @@ rm -rf ./node0
 rm -rf ./node1
 rm -rf ./node2
 rm -rf ./node3
+rm -rf ./node4
 
 # init node
 sesame init node0 --chain-id $CHAIN_ID --home ./node0
 sesame init node1 --chain-id $CHAIN_ID --home ./node1
 sesame init node2 --chain-id $CHAIN_ID --home ./node2
 sesame init node3 --chain-id $CHAIN_ID --home ./node3
+sesame init node4 --chain-id $CHAIN_ID --home ./node4
 
 # config
 sesame config chain-id $CHAIN_ID --home ./node0
@@ -35,6 +38,9 @@ sesame config chain-id $CHAIN_ID --home ./node3
 sesame config keyring-backend test --home ./node3
 sesame config broadcast-mode block --home ./node3
 
+sesame config chain-id $CHAIN_ID --home ./node4
+sesame config keyring-backend test --home ./node4
+sesame config broadcast-mode block --home ./node4
 
 # add keys
 echo $MNEMONIC0 | sesame keys add node0 --home ./node0 --recover
@@ -49,6 +55,9 @@ echo $MNEMONIC2 | sesame keys add node2 --home ./node2 --recover
 echo $MNEMONIC3 | sesame keys add node3 --home ./node0 --recover
 echo $MNEMONIC3 | sesame keys add node3 --home ./node3 --recover
 
+echo $MNEMONIC4 | sesame keys add node4 --home ./node0 --recover
+echo $MNEMONIC4 | sesame keys add node4 --home ./node4 --recover
+
 # add genesis account
 sesame add-genesis-account node0 10000000ssc --home ./node0
 sesame add-genesis-account treasurer 100000000ssc --home ./node0
@@ -62,6 +71,10 @@ sesame add-genesis-account node2 10000000ssc --home ./node2
 sesame add-genesis-account node3 10000000ssc --home ./node0
 sesame add-genesis-account node3 10000000ssc --home ./node3
 
+sesame add-genesis-account node4 10000000ssc --home ./node0
+sesame add-genesis-account node4 10000000ssc --home ./node4
+
+
 # add gentx validator
 sesame gentx node0 5000000ssc --home ./node0 --keyring-backend test --chain-id $CHAIN_ID --moniker node0
 
@@ -71,10 +84,13 @@ sesame gentx node2 5000000ssc --home ./node2 --keyring-backend test --chain-id $
 
 sesame gentx node3 5000000ssc --home ./node3 --keyring-backend test --chain-id $CHAIN_ID --moniker node3
 
+sesame gentx node4 5000000ssc --home ./node4 --keyring-backend test --chain-id $CHAIN_ID --moniker node4
+
 # copy genesis file to node0
 cp ./node1/config/gentx/* ./node0/config/gentx
 cp ./node2/config/gentx/* ./node0/config/gentx
 cp ./node3/config/gentx/* ./node0/config/gentx
+cp ./node4/config/gentx/* ./node0/config/gentx
 
 # collect gentxs
 sesame collect-gentxs --home ./node0
@@ -83,19 +99,23 @@ sesame collect-gentxs --home ./node0
 cp ./node0/config/genesis.json ./node1/config/genesis.json
 cp ./node0/config/genesis.json ./node2/config/genesis.json
 cp ./node0/config/genesis.json ./node3/config/genesis.json
+cp ./node0/config/genesis.json ./node4/config/genesis.json
 
 NODE0ID=$(sesame tendermint show-node-id --home ./node0)
 NODE1ID=$(sesame tendermint show-node-id --home ./node1)
 NODE2ID=$(sesame tendermint show-node-id --home ./node2)
 NODE3ID=$(sesame tendermint show-node-id --home ./node3)
+NODE4ID=$(sesame tendermint show-node-id --home ./node4)
 
 cat > ../.env << EOF
 NODE0IP=snode0
 NODE1IP=snode1
 NODE2IP=snode2
 NODE3IP=snode3
+NODE4IP=snode4
 NODE0ID=$NODE0ID
 NODE1ID=$NODE1ID
 NODE2ID=$NODE2ID
 NODE3ID=$NODE3ID
+NODE4ID=$NODE4ID
 EOF
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/cosmos/cosmos-sdk v0.45.5
 	github.com/cosmos/ibc-go/v3 v3.0.1
 	github.com/gogo/protobuf v1.3.3
-	github.com/golang/protobuf v1.5.2
+	github.com/golang/protobuf v1.5.3
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/ignite/cli v0.23.0
@@ -17,7 +17,7 @@ require (
 	github.com/tendermint/spn v0.2.1-0.20220708132853-26a17f03c072
 	github.com/tendermint/tendermint v0.34.19
 	github.com/tendermint/tm-db v0.6.7
-	google.golang.org/genproto v0.0.0-20230221151758-ace64dc21148
+	google.golang.org/genproto v0.0.0-20230223222841-637eb2293923
 	google.golang.org/grpc v1.53.0
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -76,7 +76,6 @@ require (
 	github.com/goccy/go-yaml v1.9.4 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
 	github.com/gogo/gateway v1.1.0 // indirect
-	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/btree v1.0.0 // indirect
@@ -86,7 +85,6 @@ require (
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.1 // indirect
 	github.com/gsterjov/go-libsecret v0.0.0-20161001094733-a6f4afe4910c // indirect
 	github.com/gtank/merlin v0.1.1 // indirect
 	github.com/gtank/ristretto255 v0.1.2 // indirect
